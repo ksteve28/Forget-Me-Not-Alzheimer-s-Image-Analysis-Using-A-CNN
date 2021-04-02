c4b5336240e5f88a0070c7f4c52c83b3fbc189df
ksteve28 <ksteve@live.com>
2021-04-01 18:54:29 -0600

    edits on notebooks, scripts, and readme

--- src/cnn.py
+++ src/cnn.py
@@ -63,31 +63,160 @@ def dense_block(units, dropout_rate):
     
     return block
 
+def build_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
+        tf.keras.Input(shape=(*image_size, 3)),
+        #First Layer
+        tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same'),
+        tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same'),
+        tf.keras.layers.MaxPool2D(),
+        #Second Layer
+        conv_block(32),
+        #Third Layer
+        conv_block(64),
+        #Fourth Layer
+        conv_block(128),
+        tf.keras.layers.Dropout(0.2),
+
+        #Full Connected Layers 
+        tf.keras.layers.Flatten(),
+        dense_block(128, 0.5),
+        dense_block(64, 0.3),
+        #Output layer
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    return model
+
+
+  
+    def model_summary(model):
+        return model.summary()
+
+history = model.fit(
+    train_data,
+    validation_data=val_data,
+#     callbacks=[lr_scheduler],
+    epochs=epoch
+)
+
+#Easy to combine the graphs, although for visualization purpose they are split
+
+def graph_performance():
+    acc = history.history['accuracy']
+    recall = history.history['recall']
+    precision = history.history['precision']
+
+    epochs_range = range(epoch)
+
+    plt.figure(figsize=(8,8))
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, recall, label='Training Recall')
+    plt.plot(epochs_range, precision, label='Training Precision')
+    plt.legend(loc='lower right')
+    plt.xlabel('Epochs')
+    plt.ylabel('Percentage Increased')
+    plt.title('Training Performance Metrics')
+    plt.savefig('../images/performance.jpg')
+    plt.tight_layout()
+    plt.show()
+
+
+def graph_loss():
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epoch)
+    plt.figure(figsize=(8,8))
+    plt.subplot(1, 3, 2)
+    plt.plot(epochs_range, loss, label='Training Loss') 
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.xlabel('Epochs')
+    plt.ylabel('Percentage Decreased')
+    plt.title('Loss')
+    plt.savefig('../images/loss.jpg')
+    plt.tight_layout()
+    plt.show()
+
+def graph_false_negs(): 
+    #False negatives label may change due to rerunning the code.   
+    false_neg = history.history['false_negatives_7']
+    epochs_range = range(epoch)
+
+    plt.figure(figsize=(8,8))
+    plt.subplot(1, 3, 3)
+    plt.plot(epochs_range, false_neg, label='False Negative Loss')
+    plt.legend(loc='upper right')
+    plt.xlabel('Epochs')
+    plt.ylabel('Number of False Negative Loss')
+    plt.title('FN Loss')
+    plt.savefig('../images/false_negs.jpg')
+    plt.tight_layout()
+
+    plt.show()
+
+
+
 
 
     if __name__ == "__main__":
 
+
         val_data = tf.keras.preprocessing.image_dataset_from_directory(
-        '../Alzheimer_s Dataset/train',
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=image_size,
-        batch_size=batch_size,
-        )
+                '../Alzheimer_s Dataset/train',
+                validation_split=0.2,
+                subset="validation",
+                seed=123,
+                image_size=image_size,
+                batch_size=batch_size,
+                )
+        train_data = tf.keras.preprocessing.image_dataset_from_directory(
+                '../Alz_data/train/',
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=image_size,
+            batch_size=batch_size,
+            )
 
 
-model = build_model()
 
-METRICS = [
-        'accuracy',
-        tf.keras.metrics.Precision(name='precision'),
-        tf.keras.metrics.Recall(name='recall'), 
-        tf.keras.metrics.FalseNegatives()
-    ]
+        test_data = tf.keras.preprocessing.image_dataset_from_directory(
+                '../Alz_data/test/',
+                mage_size=image_size,
+                batch_size=batch_size,
+                )
+
+
+
+
+        model = build_model()
+
+        METRICS = [
+            'accuracy',
+            tf.keras.metrics.Precision(name='precision'),
+            tf.keras.metrics.Recall(name='recall'), 
+            tf.keras.metrics.FalseNegatives()
+            ]
     
-model.compile(
-        optimizer='adam',
-        loss=tf.losses.BinaryCrossentropy(),
-        metrics=METRICS
-    )
+        model.compile(
+            optimizer='adam',
+            loss=tf.losses.BinaryCrossentropy(),
+            metrics=METRICS
+            )
+        
+        history = model.fit(
+                    train_data,
+                validation_data=val_data,
+                epochs=epoch
+                )
+
+        test_loss, test_acc, test_precision, test_recall, test_false = model.evaluate(test_data)
+
+        print('\nTest accuracy: {:.2f}'.format(test_acc),
+                '\nTest recall: {:.2f}'.format(test_recall),
+                '\nTest precision: {:.2f}'.format(test_precision),
+                '\nTest False Negatives: {:.2f}'.format(test_false),
+                '\nTest loss: {:.2f}'.format(test_loss))
